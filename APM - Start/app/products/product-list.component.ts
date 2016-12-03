a8fb24650428e4cef3127d20d6850906b4ea360b
jbont <j@b.c>
2016-12-03 23:08:04 +0100

    Price format

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -15,7 +15,7 @@ export class ProductListComponent {
             "productCode": "GDN-0011",
             "releaseDate": "March 19, 2016",
             "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
+            "price": 19.5,
             "starRating": 3.2,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
         },
