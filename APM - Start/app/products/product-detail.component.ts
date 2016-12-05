2a20252e0cb56aaeec29754fcf84a7ee7ba607e0
jbont <j@b.c>
2016-12-05 20:41:04 +0100

    Display route parameter (product id) in product-detail.component.html title Route with code using router.navigate method

--- APM - Start/app/products/product-detail.component.ts
+++ APM - Start/app/products/product-detail.component.ts
@@ -1,17 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {IProduct} from "./product";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 @Component(
     {
         moduleId: module.id,
         templateUrl: "product-detail.component.html"
     }
 )
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
     pageTitle: string = "Product Detail";
     product: IProduct;
 
-    constructor(private _route: ActivatedRoute) {
-        console.log(this._route.snapshot.params['id']);
+    constructor(private _route: ActivatedRoute, private _router: Router) {
+    }
+
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
+
+    onBack(): void {
+        this._router.navigate(['/products']);
     }
 }
