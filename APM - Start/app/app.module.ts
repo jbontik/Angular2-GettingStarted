96f9a9a117744af575dad24177ab3b79577effde
jbont <j@b.c>
2016-12-05 19:32:17 +0100

    Add router module to app module imports

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -6,20 +6,26 @@ import {ProductListComponent}  from './products/product-list.component';
 import {FormsModule} from "@angular/forms";
 import {ProductFilterPipe} from "./products/product-filter.pipe";
 import {StarComponent} from "./shared/star.component";
-import {ProductService} from "./products/product.service";
 import {HttpModule} from "@angular/http";
 import {ProductDetailComponent} from "./products/product-detail.component";
 import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
     // needs FormsModule for two way binding
-    imports: [BrowserModule, FormsModule, HttpModule],
+    imports: [
+        BrowserModule
+        , FormsModule
+        , HttpModule
+        , RouterModule
+    ],
     declarations: [AppComponent
         , ProductListComponent
         , ProductFilterPipe
         , StarComponent
         , WelcomeComponent
-        , ProductDetailComponent],
+        , ProductDetailComponent
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
