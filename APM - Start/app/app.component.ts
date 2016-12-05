37be6fabd00c5f1341fd85a276f32f2f51c192e1
jbont <j@b.c>
2016-12-05 19:59:31 +0100

    Add router-outlet to finalize plumbing for routing

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -14,6 +14,7 @@ import {ProductService} from "./products/product.service";
                 </ul>
             </div>
         </nav>
+        <div class="container"><router-outlet></router-outlet></div>
     </div>
     `,
     // resgisters a provider for the ProductService
