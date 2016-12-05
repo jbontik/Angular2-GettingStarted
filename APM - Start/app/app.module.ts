a43d7ec493ac3c994193a770f74d0b25d64e1d8f
jbont <j@b.c>
2016-12-05 19:31:09 +0100

    Add product-detail.component to app module declarations

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -8,11 +8,18 @@ import {ProductFilterPipe} from "./products/product-filter.pipe";
 import {StarComponent} from "./shared/star.component";
 import {ProductService} from "./products/product.service";
 import {HttpModule} from "@angular/http";
+import {ProductDetailComponent} from "./products/product-detail.component";
+import {WelcomeComponent} from "./home/welcome.component";
 
 @NgModule({
     // needs FormsModule for two way binding
     imports: [BrowserModule, FormsModule, HttpModule],
-    declarations: [AppComponent, ProductListComponent, ProductFilterPipe, StarComponent],
+    declarations: [AppComponent
+        , ProductListComponent
+        , ProductFilterPipe
+        , StarComponent
+        , WelcomeComponent
+        , ProductDetailComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
